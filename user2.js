7ed52b5b9bacc600fed10389855e3e479f4370a0
Gopi4596 <86945586+Gopi4596@users.noreply.github.com>
2021-07-05 11:23:14 +0530

    Add files via upload

---
+++ user2.js
@@ -0,0 +1,14 @@
+function validate()
+{
+    var username=document.getElementById("username").value;
+    var password=document.getElementById("password").value;
+    if(username=="admin"&&password=="admin")
+    {
+        alert("Login Succesfull");
+        return false;
+    }
+    else
+    {
+        alert("Login Failed");
+    }
+}
